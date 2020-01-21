e0b6e2d7340352b9f7e52679bc9a7827558108a9
alan <mac@192.168.0.100>
2020-01-21 15:33:12 +0800

    单调栈结构-有重复值

--- 单调栈结构.js
+++ 单调栈结构.js
@@ -21,6 +21,20 @@
  * 给定一个可能含有重复值的数组arr，找到每一个i位置左边和右边离i位置最近且值比arr[i]小的位置。返回所有位置相应的信息。
  * 【要求】
  * 如果arr长度为N，实现原问题和进阶问题的解法，时间复杂度都达到O(N)。
+ * 【举例】
+ * arr = {3, 1, 3, 4, 3, 5, 3, 2, 2}
+ * 返回如下二维数组作为结果：
+ * {
+ *    {-1,-1},
+ *    {-1,-1},
+ *    { 1, 7},
+ *    { 2, 4},
+ *    { 1, 7},
+ *    { 4, 6},
+ *    { 1, 7}
+ *    { 1,-1}
+ *    { 1,-1}
+ * }
  * **/
 function getNearNoRepeat(arr) {
     let res = []; // 返回结果二维数组
@@ -41,4 +55,37 @@ function getNearNoRepeat(arr) {
     return res;
 }
 
-console.log(getNearNoRepeat([3, 4, 1, 5, 6, 2, 7]))
+// console.log(getNearNoRepeat([3, 4, 1, 5, 6, 2, 7]))
+
+// 进阶问题
+function getNearRepeat(arr) {
+    let res = []; // 返回结果二维数组
+    let singleStack = []; // 单调栈
+    for (let i = 0; i < arr.length; i++) {
+        while (singleStack.length !== 0 && arr[i] < arr[singleStack[singleStack.length - 1][0]]) {
+            let popIndex = singleStack.pop();
+            let singleStackPeek = singleStack[singleStack.length - 1];
+            let leftIndex = singleStack.length === 0 ? -1 : singleStackPeek[singleStackPeek.length - 1];
+            for (let j = 0; j < popIndex.length; j++) {
+                res[popIndex[j]] = [leftIndex, i];
+            }
+        }
+        if (singleStack.length === 0 || arr[i] > arr[singleStack[singleStack.length - 1][0]]) {
+            singleStack.push([i]);
+        } else if (arr[i] === arr[singleStack[singleStack.length - 1][0]]) {
+            let stackTop = singleStack.pop();
+            stackTop.push(i);
+            singleStack.push(stackTop);
+        }
+    }
+    while (singleStack.length !== 0) {
+        let popIndex = singleStack.pop();
+        let singleStackPeek = singleStack[singleStack.length - 1];
+        let leftIndex = singleStack.length === 0 ? -1 : singleStackPeek[singleStackPeek.length - 1];
+        for (let i = 0; i < popIndex.length; i++) {
+            res[popIndex[i]] = [leftIndex, -1];
+        }
+    }
+    return res;
+}
+console.log(getNearRepeat([3, 1, 3, 4, 3, 5, 3, 2, 2]))
