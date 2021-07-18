d122898169a3075e9b1e789c4a65f2fc946f1ad3
Alan <wyffcld@163.com>
2021-07-18 22:57:16 +0800

    猫狗队列，优化题目说明

--- 猫狗队列.js
+++ 猫狗队列.js
@@ -1,4 +1,5 @@
 // 宠物、狗和猫的构造函数如下：
+
 // 宠物
 function Pet(type) {
     this.type = type; // 宠物类型
@@ -8,24 +9,26 @@ Pet.prototype.getPetType = function() {
 }
 
 // 猫
-function Cat() {
+function Cat(name) {
     Pet.call(this, "cat");
+    this.name = name
 }
 Cat.prototype = new Pet();
 Cat.prototype.constructor = Cat;
 
 // 狗
-function Dog() {
+function Dog(name) {
     Pet.call(this, "dog");
+    this.name = name
 }
 Dog.prototype = new Pet();
 Dog.prototype.constructor = Dog;
 /***
  * 实现一种猫狗队列的结构，要求如下：
 用户可以调用add方法将cat类或者dog类的实例放入队列中；
-用户可以调用pollAll方法，将队列中所有的实例按照进队列的先后顺序依次弹出；
-用户可以调用pollDog方法，将队列中dog类的实例按照进队列的先后顺序依次弹出；
-用户可以调用pollCat方法，将队列中cat类的实例按照进队列的先后顺序依次弹出；
+用户可以调用pollAll方法，将队列中所有的实例按照进队列的先后顺序依次弹出,每次调用弹出一个；
+用户可以调用pollDog方法，将队列中dog类的实例按照进队列的先后顺序依次弹出,每次调用弹出一个；
+用户可以调用pollCat方法，将队列中cat类的实例按照进队列的先后顺序依次弹出,每次调用弹出一个；
 用户可以调用isEmpty方法，检查队列中是否还有dog和cat的实例；
 用户可以调用isDogEmpty方法，检查队列中是否还有dog的实例；
 用户可以调用isCatEmpty方法，检查队列中是否还有cat的实例；
@@ -54,9 +57,9 @@ function DogCatQueue() {
     this.count = 0;
 }
 DogCatQueue.prototype.add = function(pet) {
-    if(pet.getPetType === "dog") {
+    if(pet.getPetType() === "dog") {
         this.dogQ.push(new PetEnterQueue(pet, this.count++));
-    }else if(pet.getPetType === "cat") {
+    }else if(pet.getPetType() === "cat") {
         this.catQ.push(new PetEnterQueue(pet, this.count++));
     }else {
         throw new Error("err, not dog or cat");
@@ -105,6 +108,23 @@ DogCatQueue.prototype.isDogEmpty = function() {
     return  this.dogQ.length === 0;
 }
 
-DogCatQueue.prototype.iscatEmpty = function() {
+DogCatQueue.prototype.isCatEmpty = function() {
     return this.catQ.length === 0;
 }
+
+// 测试用例
+let cat1 = new Cat("cat1")
+let cat2 = new Cat("cat2")
+let cat3 = new Cat("cat3")
+let dog1 = new Dog("dog1")
+let dog2 = new Dog("dog2")
+let dog3 = new Dog("dog3")
+
+let catDogQ = new DogCatQueue();
+catDogQ.add(cat1)
+catDogQ.add(dog1)
+catDogQ.add(cat2)
+catDogQ.add(dog2)
+catDogQ.add(dog3)
+catDogQ.add(cat3)
+
